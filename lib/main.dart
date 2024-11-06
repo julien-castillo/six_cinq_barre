bac262abe82cab247ddeb33de0d7d480147d5972
Julien Castillo <juliencastillo77@gmail.com>
2024-11-06 21:32:16 +0100

    fix shared_prefrences

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:app_six_cinq_barre/pages/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:app_six_cinq_barre/gsheet_setup.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await gSheetInit();
   await initializeDateFormatting('fr_FR', null);
+  await SharedPreferences.getInstance();
 
   runApp(const MyApp());
 }
